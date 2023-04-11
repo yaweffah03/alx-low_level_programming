d0c09d45945babeca694e254531199ac49e25946
Charles Effah <mr.effah@icloud.com>
2023-04-11 14:23:15 -0700

    task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,17 +6,17 @@
   */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
+	int n;
 	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	for (n = 0; b[n]; n++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[n] < '0' || b[n] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[n] - '0');
 	}
 
 	return (dec_val);
